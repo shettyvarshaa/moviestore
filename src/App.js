14e0e3bfd806bf663b0633b08bdafd1f3880b772
shettyvarshaa <shettyvarshaa275@gmail.com>
2024-08-24 00:25:49 +0530

    Created MovieStore

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import { useEffect, useState } from "react";
+import React from "react";
+import MovieCard from "./MovieCard";
+//8a2cd6af
+import "./App.css";
+import SearchIcon from "./search.svg";
+const API_URL = "https://www.omdbapi.com/?apikey=8a2cd6af";
+
+const App = () => {
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    setMovies(data.Search);
+  };
+  useEffect(() => {
+    searchMovies("pokemon");
+  }, []);
+
+  return (
+    <div className="app">
+      <h1>MovieStore</h1>
+      <div className="search">
+        <input
+          placeholder="Search for movies"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <img src={SearchIcon} alt="search" onClick={() => searchMovies(searchTerm)} />
+      </div>
+
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <MovieCard movie={movie} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>No movies found</h2>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default App;
